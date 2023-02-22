645894c463fa073894337ce63820fcacefcc433b
Teemukissa <tremppu@hotmail.com>
2023-02-22 12:25:46 +0200

    feat: write first tests!

--- libs/margarita-form/src/lib/margarita-form.spec.ts
+++ libs/margarita-form/src/lib/margarita-form.spec.ts
@@ -1,7 +1,57 @@
-import { margaritaForm } from './margarita-form';
+import { MargaritaForm } from './margarita-form';
+import { MargaritaFormField } from './margarita-form-types';
+
+const fieldNameInitialValue = 'Hello world';
+
+const commonField: MargaritaFormField = {
+  name: 'fieldName',
+  initialValue: fieldNameInitialValue,
+};
+
+const groupField: MargaritaFormField = {
+  name: 'groupName',
+  fields: [commonField],
+};
+
+const arrayField: MargaritaFormField = {
+  name: 'arrayName',
+  repeatable: true,
+  fields: [commonField],
+};
 
 describe('margaritaForm', () => {
-  it('should work', () => {
-    expect(margaritaForm()).toEqual('margarita-form');
+  it('Create basic schema', () => {
+    const form = new MargaritaForm({ fields: [commonField] });
+    expect(form.controls).toHaveProperty(['fieldName']);
+    expect(form.value).toHaveProperty(['fieldName'], fieldNameInitialValue);
+  });
+  it('Create group schema', () => {
+    const form = new MargaritaForm({
+      fields: [groupField],
+    });
+    expect(form.controls).toHaveProperty([
+      'groupName',
+      'controls',
+      'fieldName',
+    ]);
+    expect(form.value).toHaveProperty(
+      ['groupName', 'fieldName'],
+      fieldNameInitialValue
+    );
+  });
+  it('Create array schema', () => {
+    const form = new MargaritaForm({
+      fields: [arrayField],
+    });
+    expect(form.controls).toHaveProperty([
+      'arrayName',
+      'controlsArray',
+      '0',
+      'fieldName',
+    ]);
+    expect(form.value).toHaveProperty(
+      ['arrayName', '0', 'fieldName'],
+      fieldNameInitialValue
+    );
   });
 });
