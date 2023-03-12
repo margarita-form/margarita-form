506a9895c6805fb49ce4dcd656a9b8c79db6be4d
Teemu <tremppu@hotmail.com>
2023-03-12 13:25:28 +0200

    refactor: useEffect to useSyncExternalStore

--- libs/margarita-form/src/lib/react/margarita-form-hooks.ts
+++ libs/margarita-form/src/lib/react/margarita-form-hooks.ts
@@ -1,41 +1,62 @@
-import type {
-  MargaritaFormOptions,
-  MargaritaFormState,
-} from '../margarita-form-types';
-import { useEffect, useId, useMemo, useState } from 'react';
+import type { MargaritaFormOptions } from '../margarita-form-types';
+import { useId, useLayoutEffect, useSyncExternalStore } from 'react';
 import { createMargaritaForm, MargaritaForm } from '../margarita-form';
+import { combineLatest, debounceTime } from 'rxjs';
 
-declare global {
-  interface Window {
-    [key: string]: MargaritaForm<unknown>;
-  }
+const forms: Record<string, MargaritaForm<unknown>> = {};
+
+const getForm = <T>(formId: string, options: MargaritaFormOptions) => {
+  if (formId in forms) return forms[formId] as MargaritaForm<T>;
+  const newForm = createMargaritaForm(options);
+  forms[formId] = newForm;
+  return newForm as MargaritaForm<T>;
+};
+
+interface FormStore<T> {
+  form: MargaritaForm<T>;
+  getSnapshot: () => unknown;
+  subscribe: (listener: () => void) => () => void;
 }
+
+const stores: Record<string, FormStore<unknown>> = {};
+
+const createFormStore = <T>(
+  id: string,
+  options: MargaritaFormOptions
+): FormStore<T> => {
+  const form = getForm<T>(id, options);
+
+  const subscribe = (listener: () => void) => {
+    const changes = combineLatest([form.valueChanges, form.stateChanges]);
+    const subscription = changes.pipe(debounceTime(50)).subscribe(() => {
+      form.updateSyncId();
+      listener();
+    });
+    return () => subscription.unsubscribe();
+  };
+
+  const getSnapshot = () => {
+    return form.syncId;
+  };
+
+  return {
+    form,
+    getSnapshot,
+    subscribe,
+  };
+};
 
 export const useMargaritaForm = <T = unknown>(
   options: MargaritaFormOptions
 ) => {
   const formId = useId();
-
-  const form: MargaritaForm<T> = useMemo(() => {
-    const hasWindow = typeof window !== 'undefined';
-    if (hasWindow && formId in window)
-      return window[formId] as MargaritaForm<T>;
-    const newForm = createMargaritaForm(options);
-    if (hasWindow) window[formId] = newForm;
-    return newForm as MargaritaForm<T>;
-  }, [options]);
-
-  const [value, setValue] = useState<T>({} as T);
-  const [state, setState] = useState<MargaritaFormState | null>(null);
-
-  useEffect(() => {
-    const valueChangesSubscription = form.valueChanges.subscribe(setValue);
-    const stateChangesSubscription = form.stateChanges.subscribe(setState);
-    return () => {
-      valueChangesSubscription.unsubscribe();
-      stateChangesSubscription.unsubscribe();
-    };
-  }, []);
-
-  return { form, value, state };
+  if (!stores[formId]) stores[formId] = createFormStore(formId, options);
+  const store = stores[formId] as FormStore<T>;
+  useSyncExternalStore(store.subscribe, () => store.getSnapshot());
+  const { form } = store;
+  return {
+    form,
+    value: form.value,
+    state: form.state,
+  };
 };
