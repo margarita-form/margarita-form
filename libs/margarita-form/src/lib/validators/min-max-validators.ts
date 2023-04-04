e387b836c7877d3385f8462e6b1552d8fdedb123
Teemu <tremppu@hotmail.com>
2023-04-04 11:13:49 +0300

    fix(core): min and max validators not working

--- libs/margarita-form/src/lib/validators/min-max-validators.ts
+++ libs/margarita-form/src/lib/validators/min-max-validators.ts
@@ -1,19 +1,24 @@
 import { MargaritaFormValidatorFunction } from '../margarita-form-types';
 
+const invalids: unknown[] = [undefined, null, NaN];
+
+const compare = (value: unknown, comparisonFn: (value: number) => boolean) => {
+  if (typeof value === 'number') return comparisonFn(value);
+  if (typeof value === 'string') return comparisonFn(value.trim().length);
+  if (Array.isArray(value)) return comparisonFn(value.length);
+  if (typeof value === 'object' && value !== null)
+    comparisonFn(Object.keys(value).length);
+  return true;
+};
+
 export const minValidator: (
   errorMessage?: string
 ) => MargaritaFormValidatorFunction<number> =
   (errorMessage = 'Value is too low!') =>
   ({ value, params: minValue }) => {
-    if (!minValue) return { valid: true };
-    let valueIsInvalid = false;
-    if (!valueIsInvalid && value) {
-      if (typeof value === 'number') value < minValue;
-      if (typeof value === 'string')
-        valueIsInvalid = value.trim().length < minValue;
-      if (Array.isArray(value)) valueIsInvalid = value.length < minValue;
-      if (typeof value === 'object') Object.keys(value).length < minValue;
-    }
+    if (invalids.includes(minValue)) return { valid: true };
+    if (invalids.includes(value)) return { valid: true };
+    const valueIsInvalid = compare(value, (number) => number < minValue);
     const error = valueIsInvalid ? errorMessage : null;
     return { valid: !valueIsInvalid, error };
   };
@@ -22,16 +27,10 @@ export const maxValidator: (
   errorMessage?: string
 ) => MargaritaFormValidatorFunction<number> =
   (errorMessage = 'Value is too high!') =>
-  ({ value, params: minValue }) => {
-    if (!minValue) return { valid: true };
-    let valueIsInvalid = false;
-    if (!valueIsInvalid && value) {
-      if (typeof value === 'number') value > minValue;
-      if (typeof value === 'string')
-        valueIsInvalid = value.trim().length > minValue;
-      if (Array.isArray(value)) valueIsInvalid = value.length > minValue;
-      if (typeof value === 'object') Object.keys(value).length > minValue;
-    }
+  ({ value, params: maxValue }) => {
+    if (invalids.includes(maxValue)) return { valid: true };
+    if (invalids.includes(value)) return { valid: true };
+    const valueIsInvalid = compare(value, (number) => number > maxValue);
     const error = valueIsInvalid ? errorMessage : null;
     return { valid: !valueIsInvalid, error };
   };
