2fe63df6899e57386cf5bf02b1177c175d72c4ce
Teemu <tremppu@hotmail.com>
2023-04-24 22:58:15 +0300

    feat(core): add attributes property that gets resolved similar to params but values are added to refs

--- libs/margarita-form/src/lib/managers/margarita-form-ref-manager.ts
+++ libs/margarita-form/src/lib/managers/margarita-form-ref-manager.ts
@@ -4,6 +4,7 @@ import { MFC, MargaritaFormBaseElement } from '../margarita-form-types';
 import { handleFormElementSubmit } from './ref-manager-helpers/margarita-form-ref-form-submit';
 import { setControlValidationFromNode } from './ref-manager-helpers/margarita-form-ref-set-control-validation';
 import {
+  handleControlAttributeChanges,
   handleControlDisable,
   handleControlReadonly,
   handleElementBlur,
@@ -14,21 +15,29 @@ import {
   setControlValueOnNodeValueChanges,
 } from './ref-manager-helpers/margarita-form-ref-value-changes';
 
+interface RefEntry {
+  node: MargaritaFormBaseElement;
+  cleanup: () => void;
+}
+
 class RefManager<CONTROL extends MFC> extends BaseManager {
-  #refs: MargaritaFormBaseElement<CONTROL>[] = [];
+  #refs: RefEntry[] = [];
   public changes = new BehaviorSubject<MargaritaFormBaseElement<CONTROL>[]>([]);
 
   constructor(public control: CONTROL) {
     super();
 
     this.onCleanup = () => {
-      this.#refs.forEach((ref) => {
-        if (!ref || !ref.controls) return;
-        const { controls = [] } = ref;
+      this.#refs.forEach(({ node, cleanup }) => {
+        if (!node || !node.controls) return;
+        const { controls = [] } = node;
         const index = controls.findIndex(
           (control: MFC) => control.key === this.control.key
         );
-        if (index > -1) ref.controls.splice(index, 1);
+        if (index > -1) {
+          node.controls.splice(index, 1);
+          cleanup();
+        }
       });
     };
   }
@@ -64,21 +73,26 @@ class RefManager<CONTROL extends MFC> extends BaseManager {
     const handleBlur = handleElementBlur(params);
     const handleFocus = handleElementFocus(params);
     const handleSubmit = handleFormElementSubmit(params);
+    const handleAttributes = handleControlAttributeChanges(params);
+
+    const unsubscribe = () => {
+      this.#disconnectNodeFromControl(node);
+      handleControlValueChange?.unsubscribe();
+      handleNodeValueChange?.unsubscribe();
+      handleDisable?.unsubscribe();
+      handleReadOnly?.unsubscribe();
+      handleBlur?.unsubscribe();
+      handleFocus?.unsubscribe();
+      handleSubmit?.unsubscribe();
+      handleAttributes?.unsubscribe();
+      mutationObserver.disconnect();
+    };
 
     const mutationObserver = new MutationObserver((events) => {
       events.forEach((event) => {
         event.removedNodes.forEach((removedNode) => {
           if (removedNode === node) {
-            this.#disconnectNodeFromControl(node);
-            handleControlValueChange?.unsubscribe();
-            handleNodeValueChange?.unsubscribe();
-            handleDisable?.unsubscribe();
-            handleReadOnly?.unsubscribe();
-            handleBlur?.unsubscribe();
-            handleFocus?.unsubscribe();
-            handleSubmit?.unsubscribe();
-
-            mutationObserver.disconnect();
+            unsubscribe();
             this.#emitChanges();
           }
         });
@@ -88,6 +102,16 @@ class RefManager<CONTROL extends MFC> extends BaseManager {
     if (node.parentNode) {
       mutationObserver.observe(node.parentNode, { childList: true });
     }
+
+    const cleanup = () => {
+      unsubscribe();
+      mutationObserver.disconnect();
+    };
+
+    this.#refs.push({
+      node,
+      cleanup,
+    });
 
     this.#emitChanges();
   }
--- libs/margarita-form/src/lib/managers/ref-manager-helpers/margarita-form-ref-state-changes.ts
+++ libs/margarita-form/src/lib/managers/ref-manager-helpers/margarita-form-ref-state-changes.ts
@@ -1,5 +1,13 @@
-import { distinctUntilChanged, fromEvent, map } from 'rxjs';
+import {
+  combineLatest,
+  debounceTime,
+  distinctUntilChanged,
+  fromEvent,
+  map,
+  switchMap,
+} from 'rxjs';
 import type { MFC, MargaritaFormBaseElement } from '../../margarita-form-types';
+import { mapResolverEntries } from '../../helpers/resolve-function-outputs';
 
 export const handleElementBlur = <CONTROL extends MFC = MFC>({
   node,
@@ -57,5 +65,40 @@ export const handleControlReadonly = <CONTROL extends MFC = MFC>({
     )
     .subscribe((readOnly) => {
       node.readOnly = readOnly;
+    });
+};
+
+export const handleControlAttributeChanges = <CONTROL extends MFC = MFC>({
+  node,
+  control,
+}: {
+  node: MargaritaFormBaseElement<CONTROL>;
+  control: CONTROL;
+}) => {
+  return combineLatest([control.valueChanges, control.stateChanges])
+    .pipe(
+      debounceTime(1),
+      switchMap(([value]) => {
+        return mapResolverEntries('Attributes', control.field.attributes, {
+          control,
+          value,
+          params: null,
+        });
+      })
+    )
+    .subscribe((attributes) => {
+      try {
+        const el = node as HTMLElement;
+        Object.entries(attributes).forEach(([key, value]) => {
+          el.setAttribute(key, String(value));
+        });
+      } catch (error) {
+        // Could not set attributes!
+        console.error('Could not set attributes!', {
+          attributes,
+          control,
+          error,
+        });
+      }
     });
 };
