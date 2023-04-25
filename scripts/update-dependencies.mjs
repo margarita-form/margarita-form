126bd04bb7786e2af67bf06b094fc587b4bf5420
Teemu <tremppu@hotmail.com>
2023-04-25 18:01:52 +0300

    chore: update lib dependencies

--- scripts/update-dependencies.mjs
+++ scripts/update-dependencies.mjs
@@ -1,28 +1,30 @@
 import { getFile, setFile, runCommand } from './script-helpers.mjs';
 
-const rootPath = '../package.json';
-const corePath = '../libs/margarita-form/package.json';
-const reactPath = '../libs/margarita-form-react/package.json';
+// const rootPath = '../package.json';
+const corePath = '../dist/libs/margarita-form/package.json';
+const reactPath = '../dist/libs/margarita-form-react/package.json';
 
 const { version } = await getFile(corePath);
-const { dependencies } = await getFile(rootPath);
+// const { dependencies } = await getFile(rootPath);
 
-const dependenciesToUpdate = ['lodash.get', 'nanoid', 'rxjs'];
+// const dependenciesToUpdate = ['lodash.get', 'nanoid', 'rxjs'];
 const libsToUpdate = [corePath, reactPath];
 
 await Promise.all(
   libsToUpdate.map(async (dist) => {
     const contents = await getFile(dist);
 
-    const hasDependency = (name) => contents.dependencies[name];
-    const setDependency = (name, _version) =>
-      (contents.dependencies[name] = _version);
+    // const hasDependency = (name) => contents.dependencies[name];
+    const setPeerDependency = (name, _version) =>
+      (contents.peerDependencies[name] = _version);
 
     // Update core
-    if (hasDependency('@margarita-form/core')) {
-      setDependency('@margarita-form/core', `^${version}`);
+    const isCore = contents.name === '@margarita-form/core';
+    if (!isCore) {
+      setPeerDependency('@margarita-form/core', `${version}`);
     }
 
+    /*
     // Update others
     dependenciesToUpdate.forEach((dependency) => {
       if (hasDependency(dependency)) {
@@ -30,6 +32,7 @@ await Promise.all(
         setDependency(dependency, current);
       }
     });
+      */
 
     await setFile(dist, contents);
   })
