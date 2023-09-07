8d0a202500dd6b5a70c1a327ca29ec4de81dc0ad
Teemukissa <tremppu@hotmail.com>
2023-09-07 12:13:51 +0300

    chore: add lifecycle related schema test to dev app

---
+++ apps/baking-oven/src/app/fields/lifecycle.ts
@@ -0,0 +1,47 @@
+import { CustomField } from '../app';
+
+export const lifecycleFields: CustomField[] = [
+  {
+    type: 'text',
+    name: 'level-1-text',
+    title: 'Level 1 text',
+    initialValue: 'level-1-text-initial-value',
+  },
+  {
+    type: 'group',
+    name: 'level-1-group',
+    title: 'Level 1 group',
+    fields: [
+      {
+        type: 'text',
+        name: 'level-2-text',
+        title: 'Level 2 text',
+        initialValue: 'level-2-text-initial-value',
+      },
+    ],
+  },
+
+  {
+    type: 'repeatable',
+    name: 'level-1-array',
+    title: 'Level 1 array',
+    grouping: 'array',
+    // startWith: ['level-2-group', 'level-2-group', 'level-2-group'],
+    startWith: 2,
+    fields: [
+      {
+        title: 'Level 2 group',
+        name: 'level-2-group',
+        type: 'group',
+        fields: [
+          {
+            type: 'text',
+            name: 'level-3-text',
+            title: 'Level 3 text',
+            initialValue: 'level-3-text-initial-value',
+          },
+        ],
+      },
+    ],
+  },
+];
