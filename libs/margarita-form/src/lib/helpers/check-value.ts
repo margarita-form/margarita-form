fe469e440110acbc0663af1e0ca4024a9d83c0e5
Teemukissa <tremppu@hotmail.com>
2023-09-30 10:15:05 +0300

    fix(core): valueExists promises fix

--- libs/margarita-form/src/lib/helpers/check-value.ts
+++ libs/margarita-form/src/lib/helpers/check-value.ts
@@ -3,6 +3,7 @@ const invalidValues = ['', null, undefined, NaN] as unknown[];
 export const valueExists = (value: unknown) => {
   try {
     if (invalidValues.includes(value)) return false;
+    if (value instanceof Promise) return true;
     if (value && typeof value === 'object') {
       return Object.values(value).length > 0;
     }
