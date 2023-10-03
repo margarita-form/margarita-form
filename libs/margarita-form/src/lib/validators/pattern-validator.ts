092cb9277bb23847220ffd2d2eb624c8d6732067
Teemukissa <tremppu@hotmail.com>
2023-10-03 22:53:49 +0300

    chore(core): add ReDos check to pattern validator

--- libs/margarita-form/src/lib/validators/pattern-validator.ts
+++ libs/margarita-form/src/lib/validators/pattern-validator.ts
@@ -1,12 +1,22 @@
 import { MargaritaFormValidator } from '../margarita-form-types';
 
+const DEFAULT_TIMEOUT = 2000; // 2 seconds
+
+const validateRegex = (regex: RegExp, value: string, timeout: number = DEFAULT_TIMEOUT): boolean => {
+  const start = Date.now();
+  let match = false;
+  while (!match && Date.now() - start < timeout) match = regex.test(value);
+  if (!match) throw new Error('Regex is too complex and may cause ReDoS!');
+  return true;
+};
+
 export const patternValidator: (errorMessage?: string) => MargaritaFormValidator<string | RegExp> =
   (defaultErrorMessage = 'Value does not match required pattern!') =>
   ({ value, params, errorMessage = defaultErrorMessage }) => {
     if (!params || !value) return { valid: true };
-    const regex = new RegExp(params);
+    const regex = typeof params === 'string' ? new RegExp(params) : params;
     const stringValue = typeof value === 'string' ? value : JSON.stringify(value);
-    const valueIsInvalid = !regex.test(stringValue);
+    const valueIsInvalid = !validateRegex(regex, stringValue);
     const error = valueIsInvalid ? errorMessage : null;
     return { valid: !valueIsInvalid, error };
   };
