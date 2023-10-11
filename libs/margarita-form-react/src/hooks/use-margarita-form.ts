df5e44ddc68e3bae59acebfc646066edbfe00348
Teemukissa <tremppu@hotmail.com>
2023-10-11 23:52:29 +0300

    fix(react): resubscribe only if needed and actually detect field changes

--- libs/margarita-form-react/src/hooks/use-margarita-form.ts
+++ libs/margarita-form-react/src/hooks/use-margarita-form.ts
@@ -1,21 +1,28 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { useEffect, useMemo, useRef, useSyncExternalStore } from 'react';
 import { createFormStore } from '../misc/margarita-form-store';
-import { MFF, createMargaritaForm } from '@margarita-form/core';
+import { MFF, createMargaritaForm, isEqual } from '@margarita-form/core';
 
 export const useMargaritaForm = <FIELD extends MFF>(field: FIELD, useCache = true) => {
-  const fieldRef = useRef(field);
+  const fieldRef = useRef<null | FIELD>(null);
   const form = useMemo(() => {
     return createMargaritaForm<FIELD>(field, useCache);
   }, [field.name]);
 
   useEffect(() => {
+    if (!fieldRef.current) {
+      fieldRef.current = field;
+      return () => {};
+    }
     const currentForm = form;
     currentForm.resubscribe();
     return () => currentForm.cleanup();
   }, [field.name]);
 
   useEffect(() => {
-    if (fieldRef.current !== field) {
+    const { current } = fieldRef;
+    const changed = current && !isEqual(current, field, false);
+    if (changed) {
       fieldRef.current = field;
       form.updateField(field);
     }
