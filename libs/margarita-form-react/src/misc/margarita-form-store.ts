6f447a6ce4c7388b8c7b3f25f1d960ff8813f01c
Teemukissa <tremppu@hotmail.com>
2023-10-23 22:48:27 +0300

    refactor(react): update effect listeners

--- libs/margarita-form-react/src/misc/margarita-form-store.ts
+++ libs/margarita-form-react/src/misc/margarita-form-store.ts
@@ -1,13 +1,10 @@
 import type { MF } from '@margarita-form/core';
-import { BehaviorSubject, combineLatest, debounceTime } from 'rxjs';
 
 export const createFormStore = (form: MF) => {
   const subscribe = (listener: () => void) => {
-    const managerChanges = Object.values(form.managers)
-      .filter((manager) => 'changes' in manager)
-      .map((manager) => 'changes' in manager && manager.changes) as BehaviorSubject<unknown>[];
-
-    const subscription = combineLatest(managerChanges).pipe(debounceTime(10)).subscribe(listener);
+    const subscription = form.changes.subscribe(() => {
+      listener();
+    });
     return () => {
       subscription.unsubscribe();
     };
