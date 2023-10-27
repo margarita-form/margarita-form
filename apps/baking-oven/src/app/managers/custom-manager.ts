a19f4e32430e4c6c8f5c354d59bcd4f368ffe22f
Teemukissa <tremppu@hotmail.com>
2023-10-27 23:06:45 +0300

    fix(demo): add new manager params

--- apps/baking-oven/src/app/managers/custom-manager.ts
+++ apps/baking-oven/src/app/managers/custom-manager.ts
@@ -2,7 +2,7 @@ import { MF, BaseManager, MargaritaForm } from '@margarita-form/core';
 
 class CustomManager<CONTROL extends MF> extends BaseManager {
   constructor(public control: CONTROL) {
-    super();
+    super('custom-manager', control);
     if (this.control instanceof MargaritaForm) {
       console.log('Hello from custom manager!', this.control);
     }
