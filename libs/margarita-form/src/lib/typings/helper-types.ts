0e162519ff171228f3f4570ea85936b6f85ebdaa
Teemukissa <tremppu@hotmail.com>
2023-11-25 12:10:06 +0200

    fix(core): add initialize as one possible key

--- libs/margarita-form/src/lib/typings/helper-types.ts
+++ libs/margarita-form/src/lib/typings/helper-types.ts
@@ -101,7 +101,7 @@ export type I18NField<LOCALES extends string, I18NType extends object> = {
  * Type of a change event
  */
 export type ControlChange<FIELD extends MFF = MFGF, CHANGE = unknown> = {
-  name: keyof Managers;
+  name: keyof Managers | 'initialize';
   change: CHANGE;
   control: MFC<FIELD>;
   origin?: MFC<any>;
