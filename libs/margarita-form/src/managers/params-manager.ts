e9cef0e99ded90223b05ef86888ec87e8f797791
Teemukissa <tremppu@hotmail.com>
2023-12-14 19:26:16 +0200

    feat(core): Param constructor for fields to ease up param creation

--- libs/margarita-form/src/managers/params-manager.ts
+++ libs/margarita-form/src/managers/params-manager.ts
@@ -1,11 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Observable, switchMap } from 'rxjs';
 import { BaseManager, ManagerName } from './base-manager';
-import { CommonRecord, FieldParams, MFC, MFF, MargaritaFormResolver, NotFunction } from '../typings/margarita-form-types';
-import { getResolverOutputMapObservable } from '../helpers/resolve-function-outputs';
+import {
+  CommonRecord,
+  FieldParams,
+  MFC,
+  MFF,
+  MargaritaFormContextFunction,
+  MargaritaFormResolver,
+  MargaritaFormResolverOutput,
+  NotFunction,
+  ResolverParams,
+} from '../typings/margarita-form-types';
+import { getResolverOutput, getResolverOutputMapObservable, getResolverOutputMapSyncronous } from '../helpers/resolve-function-outputs';
 import { MargaritaFormControl } from '../margarita-form-control';
+import { valueIsAsync } from '../helpers/async-checks';
 
 export type Params = CommonRecord;
+
 export type MargaritaFormFieldParams = CommonRecord<NotFunction | MargaritaFormResolver<any>>;
 
 // Extends types
@@ -28,6 +40,13 @@ declare module '../typings/margarita-form-types' {
   }
 }
 
+type ParamsValue<T> = ResolverParams | MargaritaFormResolverOutput<T> | MargaritaFormResolver<T>;
+type ParamsSnapshotValue<T> = T | MargaritaFormContextFunction<T>;
+
+export class Param<T = unknown> {
+  constructor(public from: ParamsValue<T>, public snapshotValue?: ParamsSnapshotValue<T>) {}
+}
+
 export class ParamsManager<CONTROL extends MFC> extends BaseManager<Params> {
   public static override managerName: ManagerName = 'params';
   constructor(public override control: CONTROL) {
@@ -44,9 +63,15 @@ export class ParamsManager<CONTROL extends MFC> extends BaseManager<Params> {
   }
 
   public override onInitialize() {
+    const syncronousParams = this._getCurrentParams(true);
+    if (syncronousParams) {
+      this.value = syncronousParams;
+      this.emitChange(syncronousParams);
+    }
+
     const paramsSubscriptionObservable = this.control.fieldChanges.pipe(
       switchMap(() => {
-        const { params } = this.control.field;
+        const params = this._getCurrentParams(false);
         if (!params) return Promise.resolve(null);
         return getResolverOutputMapObservable(params, this.control);
       })
@@ -57,5 +82,34 @@ export class ParamsManager<CONTROL extends MFC> extends BaseManager<Params> {
       this.value = params;
       this.emitChange(params);
     });
+  }
+
+  private _getCurrentParams(snapshot = false): null | Params {
+    const { params = {}, ...rest } = this.control.field;
+    let hasParams = false;
+    const currentParams = {} as Params;
+
+    const paramsReducer =
+      (allowAny: boolean) =>
+      (acc: Params, [key, value]: [string, any]) => {
+        if (value instanceof Param) {
+          hasParams = true;
+          const fromValue = getResolverOutput({ getter: value.from, control: this.control });
+          const fromIsAsync = valueIsAsync(fromValue);
+          const syncronousValue = fromIsAsync ? value.snapshotValue : fromValue;
+          acc[key] = snapshot ? syncronousValue : value.from;
+        } else if (allowAny) {
+          hasParams = true;
+          acc[key] = value;
+        }
+        return acc;
+      };
+
+    Object.entries(params).reduce(paramsReducer(true), currentParams);
+    Object.entries(rest).reduce(paramsReducer(false), currentParams);
+
+    if (!hasParams) return null;
+    if (snapshot) return getResolverOutputMapSyncronous(currentParams, this.control);
+    return currentParams;
   }
 }
