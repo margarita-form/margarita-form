a460921813a294666681fd0c55dc234d12b37bd8
Teemukissa <tremppu@hotmail.com>
2024-01-24 22:32:17 +0200

    docs: include angular to reference docs

---
+++ libs/margarita-form-angular/src/angular.ts
@@ -0,0 +1,3 @@
+export * from './lib/margarita-form-angular.module';
+export * from './lib/margarita-form.directive';
+export * from './lib/margarita-form.service';
--- libs/margarita-form-angular/src/index.ts
+++ libs/margarita-form-angular/src/index.ts
@@ -1,5 +1,2 @@
 export * from '@margarita-form/core';
-
-export * from './lib/margarita-form-angular.module';
-export * from './lib/margarita-form.directive';
-export * from './lib/margarita-form.service';
+export * from './angular';
