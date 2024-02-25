30ec940c4b4e0bd73bf54c173c1c7fe5762c1304
Teemukissa <tremppu@hotmail.com>
2024-02-25 19:46:40 +0200

    fix(core): getControls type

--- libs/margarita-form/src/typings/margarita-form-types.ts
+++ libs/margarita-form/src/typings/margarita-form-types.ts
@@ -245,7 +245,7 @@ export interface ControlLike<FIELD extends MFF = MFF, VALUE = ControlValue<FIELD
   get activeControls(): MFC<CHILD_FIELD>[];
   get visibleControls(): MFC<CHILD_FIELD>[];
 
-  getControls<FIELD extends MFF = CHILD_FIELD>(states: string | string[]): MFC<FIELD>[];
+  getControls<FIELD extends MFF = CHILD_FIELD>(states?: string | string[]): MFC<FIELD>[];
   getSiblings<SIBLING_FIELD extends MFF = MFGF>(): MFC<SIBLING_FIELD>[];
   getActiveSiblings<SIBLING_FIELD extends MFF = MFGF>(): MFC<SIBLING_FIELD>[];
   getSibling<SIBLING_FIELD extends MFF = MFGF>(identifier: ControlIdentifier): MFC<SIBLING_FIELD>;
